a3d5565ec49c290e7d4094dba52b14ee66ce836a
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-05-24 08:31:08 -0300

    Trabalho - Criação do JS

---
+++ LandingPage/Formulário/script.js
@@ -0,0 +1,24 @@
+
+function verifiquenome() {
+    let vnome = document.forms["form"][" fnome"].value
+
+    if (vnome == "") {
+        alert ("O email é obrigatorio")
+    }
+
+}
+
+function verifiquenome() {
+    let vnome = document.forms["form"]["fnome"].value
+
+    if (vnome == "") {
+        alert ("O email é obrigatorio")
+    }
+
+}
+
+
+
+
+
+
